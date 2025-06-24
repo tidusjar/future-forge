a49eac9f18d8f39cf1b421b629915baf2004ccd4
Jamie Rees <jamie.rees@exclaimer.com>
2025-06-24 08:51:23 +0100

    ffff

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/future-forge/' : '/',
+  base: '/future-forge/',
   server: {
     host: "::",
     port: 8080,
